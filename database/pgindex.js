cc8d74d56bf32497c146facedaac315f0a8ed905
Ashvin Coca <AshvinCoca@gmail.com>
2018-11-21 12:06:58 -0800

    Adding connections

---
+++ database/pgindex.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'root',
+  port: 5432,
+});
+
+client.connect();
+
+console.log("Client successfully connected!");
+
+module.exports = client;
